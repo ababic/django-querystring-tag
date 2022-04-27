4a0bf8f2e6a7727c1b678dfacecf71b979eeb131
Andy Babic <andy.babic@torchbox.com>
2022-04-27 14:02:57 +0100

    Remove mocking from tests

--- querystring_tag/templatetags/tests.py
+++ querystring_tag/templatetags/tests.py
@@ -1,18 +1,22 @@
-from unittest.mock import MagicMock, patch
-
 from django.template import Context, Template
 from django.test import RequestFactory, SimpleTestCase
 
-from querystring_tag.nodes import QuerystringNode
-
-request_factory = RequestFactory()
 
 
-class TestTagOptionInterpretation(SimpleTestCase):
-    def render_tag(self, tag_options: str) -> MagicMock:
+
+class TestQuerystringTag(SimpleTestCase):
+    maxDiff = None
+
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.request_factory = RequestFactory()
+
+    @classmethod
+    def render_tag(cls, tag_options: str) -> str:
         template = Template("{% querystring " + tag_options + " %}")
 
-        request = request_factory.get(
+        request = cls.request_factory.get(
             "/", data={"foo": ["a", "b", "c"], "bar": [1, 2, 3], "baz": "single-value"}
         )
 
@@ -31,91 +35,88 @@ class TestTagOptionInterpretation(SimpleTestCase):
             "letter_c": "c",
             "letter_d": "c",
         }
-
-        with patch.object(QuerystringNode, "__init__") as mocked_init:
-            template.render(Context(context_data))
-            return mocked_init
+        return template.render(Context(context_data))
 
     def test_add_param_with_string(self):
-        mocked_init = self.render_tag("newparam='new'")
-        mocked_init.assert_called_once_with()
+        result = self.render_tag("newparam='new'")
+        self.assertEqual(result, "")
 
     def test_add_param_with_key_param(self):
-        mocked_init = self.render_tag("new_param_name='new'")
-        mocked_init.assert_called_once_with()
+        result = self.render_tag("new_param_name='new'")
+        self.assertEqual(result, "")
 
     def test_add_param_with_value_param(self):
-        mocked_init = self.render_tag("newparam=two")
-        mocked_init.assert_called_once_with()
+        result = self.render_tag("newparam=two")
+        self.assertEqual(result, "")
 
     def test_replace_with_string(self):
-        mocked_init = self.render_tag("foo='foo'")
-        mocked_init.assert_called_once_with()
+        result = self.render_tag("foo='foo'")
+        self.assertEqual(result, "")
 
     def test_replace_with_key_param(self):
-        mocked_init = self.render_tag("foo_parm_name='foo'")
-        mocked_init.assert_called_once_with()
+        result = self.render_tag("foo_parm_name='foo'")
+        self.assertEqual(result, "")
 
     def test_replace_with_value_param(self):
-        mocked_init = self.render_tag("foo=one")
-        mocked_init.assert_called_once_with()
+        result = self.render_tag("foo=one")
+        self.assertEqual(result, "")
 
     def test_replace_with_param_key_and_value(self):
-        mocked_init = self.render_tag("foo_parm_name=one")
-        mocked_init.assert_called_once_with()
+        result = self.render_tag("foo_parm_name=one")
+        self.assertEqual(result, "")
 
     def test_add_with_strings(self):
-        mocked_init = self.render_tag("foo+='d'")
-        mocked_init.assert_called_once_with()
+        result = self.render_tag("foo+='d'")
+        self.assertEqual(result, "")
 
     def test_add_with_key_param(self):
-        mocked_init = self.render_tag("foo_parm_name+='d'")
-        mocked_init.assert_called_once_with()
+        result = self.render_tag("foo_parm_name+='d'")
+        self.assertEqual(result, "")
 
     def test_add_with_value_param(self):
-        mocked_init = self.render_tag("foo+=letter_d")
-        mocked_init.assert_called_once_with()
+        result = self.render_tag("foo+=letter_d")
+        self.assertEqual(result, "")
 
     def test_add_with_param_key_and_value(self):
-        mocked_init = self.render_tag("foo_parm_name+=letter_d")
-        mocked_init.assert_called_once_with()
+        result = self.render_tag("foo_parm_name+=letter_d")
+        self.assertEqual(result, "")
 
     def test_remove_with_strings(self):
-        mocked_init = self.render_tag("bar-='1'")
-        mocked_init.assert_called_once_with()
+        result = self.render_tag("bar-='1'")
+        self.assertEqual(result, "")
 
     def test_remove_with_key_param(self):
-        mocked_init = self.render_tag("bar_parm_name-='1'")
-        mocked_init.assert_called_once_with()
+        result = self.render_tag("bar_parm_name-='1'")
+        self.assertEqual(result, "")
 
     def test_remove_with_value_param(self):
-        mocked_init = self.render_tag("bar-=one")
-        mocked_init.assert_called_once_with()
+        result = self.render_tag("bar-=one")
+        self.assertEqual(result, "")
 
     def test_remove_with_param_key_and_value(self):
-        mocked_init = self.render_tag("bar_parm_name-=one")
-        mocked_init.assert_called_once_with()
+        result = self.render_tag("bar_parm_name-=one")
+        self.assertEqual(result, "")
 
     def test_discard_with_strings(self):
-        mocked_init = self.render_tag("discard 'foo' 'bar'")
-        mocked_init.assert_called_once_with()
+        result = self.render_tag("discard 'foo' 'bar'")
+        self.assertEqual(result, "")
 
     def test_discard_with_params(self):
-        mocked_init = self.render_tag("discard foo_parm_name bar_param_name")
-        mocked_init.assert_called_once_with()
+        result = self.render_tag("discard foo_parm_name bar_param_name")
+        self.assertEqual(result, "")
 
     def test_dicsard_with_additional_changes(self):
-        mocked_init = self.render_tag("discard 'foo' 'bar' baz=letter_a newparam='new'")
-        mocked_init.assert_called_once_with()
+        result = self.render_tag("discard 'foo' 'bar' baz=letter_a newparam='new'")
+        self.assertEqual(result, "")
 
     def test_only_with_strings(self):
-        mocked_init = self.render_tag("only 'foo' 'bar'")
-        mocked_init.assert_called_once_with()
+        result = self.render_tag("only 'foo' 'bar'")
+        self.assertEqual(result, "")
 
     def test_only_with_params(self):
-        mocked_init = self.render_tag("only foo_parm_name bar_param_name")
-        mocked_init.assert_called_once_with()
+        result = self.render_tag("only foo_parm_name bar_param_name")
+        self.assertEqual(result, "")
 
     def test_only_with_additional_changes(self):
-        mocked_init = self.render_tag("only 'foo' 'bar' baz=letter_a newparam='new'")
-        mocked_init.assert_called_once_with()
+        result = self.render_tag("only 'foo' 'bar' baz=letter_a newparam='new'")
+        self.assertEqual(result, "")
