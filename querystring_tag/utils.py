f0fa4f35e365e7031f880a6d7ed2702194d45e05
Andy Babic <andy.babic@torchbox.com>
2022-04-29 14:41:32 +0100

    Update normalize_value() to support 'querystring_value_field' on models

--- querystring_tag/utils.py
+++ querystring_tag/utils.py
@@ -3,13 +3,19 @@ from typing import Any, Union
 from django.db.models import Model
 
 
-def normalize_value(value: Any, model_value_field: str = "pk") -> Union[str, None]:
+def normalize_value(value: Any, model_value_field: Union[None, str] = None) -> Union[str, None]:
     if value is None:
         return None
     if isinstance(value, str):
         return value
-    if isinstance(value, Model):
-        return str(getattr(value, model_value_field))
     if hasattr(value, "isoformat"):
         return value.isoformat()
+    if isinstance(value, Model):
+        if model_value_field:
+            try:
+                return str(getattr(value, model_value_field))
+            except AttributeError:
+                pass
+        field_name = getattr(value, "querystring_value_field", "pk")
+        return str(getattr(value, field_name))
     return str(value)
